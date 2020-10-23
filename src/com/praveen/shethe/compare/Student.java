26ae6d435b72d2f05a0e74cc965e8aa696dd0c7a
Praveenkumar <praveen.shethe7@gmail.com>
2020-10-23 15:00:22 +0530

    Student age based sorting

---
+++ src/com/praveen/shethe/compare/Student.java
@@ -0,0 +1,53 @@
+package com.praveen.shethe.compare;
+
+/**
+ * Created by Praveenkumar on 10/22/2020.
+ */
+public class Student implements Comparable<Student> {
+
+    Student(String name, int age, int rollNo){
+        this.name = name;
+        this.age = age;
+        this.rollNo = rollNo;
+    }
+
+    private String name;
+
+    private int age;
+
+    private int rollNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        if (age == student.getAge())
+        return 0;
+        else if ( age > student.getAge())
+            return 1;
+        else
+            return -1;
+    }
+}
---
+++ src/com/praveen/shethe/compare/TestStudent.java
@@ -0,0 +1,34 @@
+package com.praveen.shethe.compare;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by Praveenkumar on 10/22/2020.
+ */
+public class TestStudent {
+
+    public static void main(String args[]){
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+        Student student1 = new Student("Apple", 1, 1);
+        Student student2 = new Student("Ball", 2, 4);
+        Student student3 = new Student("Cat", 4, 3);
+        Student student4 = new Student("Dog", 3, 2);
+        Student student5 = new Student("Elephant", 5, 5);
+        studentArrayList.add(student1);
+        studentArrayList.add( student2);
+        studentArrayList.add(student3);
+        studentArrayList.add(student4);
+        studentArrayList.add(student5);
+        System.out.println("=====================================================================================");
+        for (Student student : studentArrayList){
+            System.out.println(student.getName() + " ========== " +student.getAge()+" ============ "+student.getRollNo());
+        }
+        Collections.sort(studentArrayList);
+        System.out.println("=====================================================================================");
+        for (Student student : studentArrayList){
+            System.out.println(student.getName() + " ========== " +student.getAge()+" ============ "+student.getRollNo());
+        }
+        System.out.println("=====================================================================================");
+    }
+}
