85179712bad6df12a28f59a4f3f8309985ce3ad4
Praveenkumar <praveen.shethe7@gmail.com>
2020-10-30 11:36:53 +0530

    insertion sorting

---
+++ src/com/praveen/shethe/sorting/InsertionSort.java
@@ -0,0 +1,33 @@
+package com.praveen.shethe.sorting;
+
+/**
+ * Created by Praveenkumar on 10/30/2020.
+ */
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] a = {10, 9, 7, 101, 23, 44, 12, 78, 34, 23};
+        System.out.println("Input provided");
+        for(int i=0;i<10;i++)
+        {
+            System.out.print(a[i]+ " ,");
+        }
+        System.out.println();
+        for(int k=1; k<10; k++)
+        {
+            int temp = a[k];
+            int j= k-1;
+            while(j>=0 && temp <= a[j])
+            {
+                a[j+1] = a[j];
+                j = j-1;
+            }
+            a[j+1] = temp;
+        }
+        System.out.println("printing sorted elements ...");
+        for(int i=0;i<10;i++)
+        {
+            System.out.print(a[i]+ " ,");
+        }
+    }
+}
