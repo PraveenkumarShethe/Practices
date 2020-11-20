e81ca3b03984307f51b99737b00a702843238dbc
Praveenkumar <praveen.shethe7@gmail.com>
2020-11-20 10:10:00 +0530

    Final Reverse Without Using Inbuilt Function

---
+++ src/com/praveen/shethe/stringoperations/FinalReverseWithoutUsingInbuiltFunction.java
@@ -0,0 +1,16 @@
+package com.praveen.shethe.stringoperations;
+
+/**
+ * Created by Praveenkumar on 11/20/2020.
+ */
+public class FinalReverseWithoutUsingInbuiltFunction {
+
+    public static void main(String[] args) {
+        String str = "Saket Saurav";
+        char chars[] = str.toCharArray();  // converted to character array and printed in reverse order
+        for(int i= chars.length-1; i > 0 ; i--) {
+            System.out.print(chars[i]);
+        }
+    }
+
+}
