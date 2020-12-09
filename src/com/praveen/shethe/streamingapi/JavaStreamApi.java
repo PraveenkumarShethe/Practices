da3caf44b71ce469542aa71ed33478e4e73115aa
Praveenkumar <praveen.shethe7@gmail.com>
2020-12-09 13:37:55 +0530

    Updated new method implementations

--- src/com/praveen/shethe/streamingapi/JavaStreamApi.java
+++ src/com/praveen/shethe/streamingapi/JavaStreamApi.java
@@ -1,8 +1,10 @@
 package com.praveen.shethe.streamingapi;
 
 import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by Praveenkumar on 12/7/2020.
@@ -29,9 +31,19 @@ public class JavaStreamApi {
         int a[] = {2,4,65,7,9,1,2,363,6};
 //        int a[] = {};
         try{
-            System.out.println(IntStream.of(a).min().getAsInt());
-            System.out.println(IntStream.of(a).max().getAsInt());
-            System.out.println(IntStream.of(a).max().getAsInt());
+            System.out.println("IntStream.of(a).min().getAsInt() : "+IntStream.of(a).min().getAsInt());
+            System.out.println(" IntStream.of(a).max().getAsInt() : "+ IntStream.of(a).max().getAsInt());
+            System.out.println("IntStream.of(a).average() : "+ IntStream.of(a).average());
+            System.out.println("IntStream.of(a).sum() : "+ IntStream.of(a).sum());
+
+            IntSummaryStatistics intSummaryStatistics = IntStream.of(a).summaryStatistics();
+            System.out.println("intSummaryStatistics.getMin() : "+intSummaryStatistics.getMin());
+            System.out.println("intSummaryStatistics.getMax() : "+ intSummaryStatistics.getMax());
+            System.out.println("intSummaryStatistics.getAverage() : "+intSummaryStatistics.getAverage());
+            System.out.println("intSummaryStatistics.getCount() : "+intSummaryStatistics.getCount());
+            System.out.println("intSummaryStatistics.getSum() : "+intSummaryStatistics.getSum());
+
+
         }catch (Exception e){
             e.printStackTrace();
         }
