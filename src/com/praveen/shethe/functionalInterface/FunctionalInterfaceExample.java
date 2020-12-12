721999009da54901a77d868f5a328bb7da8997e1
Praveenkumar <praveen.shethe7@gmail.com>
2020-12-12 19:31:59 +0530

    Functional interface example

--- src/com/praveen/shethe/functionalInterface/FunctionalInterfaceExample.java
+++ src/com/praveen/shethe/functionalInterface/FunctionalInterfaceExample.java
@@ -3,5 +3,16 @@ package com.praveen.shethe.functionalInterface;
 /**
  * Created by Praveenkumar on 12/12/2020.
  */
-public class FunctionalInterfaceExample {
+@FunctionalInterface
+interface sayable{
+    void say(String msg);
+}
+public class FunctionalInterfaceExample implements sayable{
+    public void say(String msg){
+        System.out.println(msg);
+    }
+    public static void main(String[] args) {
+        FunctionalInterfaceExample fie = new FunctionalInterfaceExample();
+        fie.say("Hello there");
+    }
 }
---
+++ src/com/praveen/shethe/functionalInterface/example/FunctionalInterfaceExample2.java
@@ -0,0 +1,22 @@
+package com.praveen.shethe.functionalInterface.example;
+
+/**
+ * Created by Praveenkumar on 12/12/2020.
+ */
+@FunctionalInterface
+interface sayable{
+    void say(String msg);   // abstract method
+    // It can contain any number of Object class methods.
+    int hashCode();
+    String toString();
+    boolean equals(Object obj);
+}
+public class FunctionalInterfaceExample2 implements sayable{
+    public void say(String msg){
+        System.out.println(msg);
+    }
+    public static void main(String[] args) {
+        FunctionalInterfaceExample2 fie = new FunctionalInterfaceExample2();
+        fie.say("Hello there");
+    }
+}
