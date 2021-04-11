1f200b439d3bc6282a3ed4f19b3e9fef3e26143e
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-04-11 11:38:03 +0530

    Multi Threading example

---
+++ src/threading/MultiThreadingMain.java
@@ -0,0 +1,48 @@
+package threading;
+
+/**
+ * Created by Praveenkumar on 4/11/2021.
+ */
+public class MultiThreadingMain {
+
+    public static void main(String[] args) {
+        Hi hi = new Hi();
+        Hello hello = new Hello();
+        hi.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        hello.start();
+    }
+}
+
+
+class Hi extends Thread{
+
+    public void run(){
+        for (int i = 0 ; i <= 5 ; i++){
+            System.out.println("Hi");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Hello extends Thread{
+
+    public void run(){
+        for (int i=0; i <=5 ; i++){
+            System.out.println("Hello");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
