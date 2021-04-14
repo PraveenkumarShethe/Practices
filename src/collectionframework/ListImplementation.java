85f048811386f027fba922d90898fc69f993e10c
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-04-14 21:57:21 +0530

    Arraylist and vector implementation

--- src/collectionframework/ListImplementation.java
+++ src/collectionframework/ListImplementation.java
@@ -1,7 +1,9 @@
 package collectionframework;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Vector;
 
 /**
@@ -13,17 +15,73 @@ public class ListImplementation {
 
         List list = new ArrayList();
         list.add(10);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(13);
+        list.add(12);
+//        list.add(15);
+//        list.add(16);
+//        list.add(17);
+//        list.add(18);
+//        list.add(19);
+//        list.add(20);
+//        list.add(19);
+        System.out.println("=============================================");
+        list.forEach(o -> {
+            System.out.println(o);
+        });
+        System.out.println("=============================================");
+        Set uniqueInt = new HashSet();
+        list.forEach(o -> {
+            uniqueInt.add(o);
+        });
+        System.out.println("=============================================");
+
+        uniqueInt.forEach(o -> {
+            System.out.println(o);
+        });
+        System.out.println("=============================================");
 
     }
 
     public void vectorimpl(){
         List list = new Vector();
         list.add(10);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(13);
+        list.add(12);
+//        list.add(15);
+//        list.add(16);
+//        list.add(17);
+//        list.add(18);
+//        list.add(19);
+//        list.add(20);
+//        list.add(19);
+        System.out.println("=============================================");
+        list.forEach(o -> {
+            System.out.println(o);
+        });
+        System.out.println("=============================================");
+        Set uniqueInt = new HashSet();
+        list.forEach(o -> {
+            uniqueInt.add(o);
+        });
+        System.out.println("=============================================");
+
+        uniqueInt.forEach(o -> {
+            System.out.println(o);
+        });
+        System.out.println("=============================================");
+
+
+
     }
 
     public static void main(String[] args) {
-
+        ListImplementation listImplementation = new ListImplementation();
+        listImplementation.vectorimpl();
     }
-
-
 }
