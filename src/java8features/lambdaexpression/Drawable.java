b279d4efe252069c17202a8687d77e2ff7b506d4
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-04-18 12:37:18 +0530

    Lambda expression

---
+++ src/java8features/lambdaexpression/Drawable.java
@@ -0,0 +1,11 @@
+package java8features.lambdaexpression;
+
+/**
+ * Created by Praveenkumar on 4/18/2021.
+ */
+@FunctionalInterface
+public interface Drawable {
+
+    void show(int val);
+
+}
--- src/java8features/lambdaexpression/Test.java
+++ src/java8features/lambdaexpression/Test.java
@@ -6,7 +6,12 @@ package java8features.lambdaexpression;
 public class Test {
 
     public static void main(String[] args) {
+        int width =10;
+        Drawable drawable = value -> {
+            System.out.println("Inside the lambda expression!!!" + width);
+        };
 
+        drawable.show(width);
     }
 
 }
