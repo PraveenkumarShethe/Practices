4347cb2b7765e9350e6b3fd61112e9668f656df8
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-02 23:01:38 +0530

    Number Pattern

--- src/javatricky/PatterPrinting.java
+++ src/javatricky/PatterPrinting.java
@@ -4,4 +4,22 @@ package javatricky;
  * Created by Praveenkumar on 5/2/2021.
  */
 public class PatterPrinting {
+
+    public static void main(String[] args) {
+        int i, j, number, n = 10;
+
+        for (i = 0; i < n; i++) {
+            number = 1;
+        //loop for columns
+            for (j = 0; j <= i; j++) {
+        //prints num
+                System.out.print(number + " ");
+        //incrementing the value of number
+                number++;
+            }
+        //throws the cursor at the next line after printing each row
+            System.out.println();
+        }
+
+    }
 }
