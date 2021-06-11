2b1b8b4786f30cf1af9f902163c71185fadb1ea5
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-06-11 22:40:46 +0530

    number test

---
+++ src/com/praveen/shethe/NumberTest.java
@@ -0,0 +1,12 @@
+package com.praveen.shethe;
+
+/**
+ * Created by Praveenkumar on 6/11/2021.
+ */
+public class NumberTest {
+
+    public static void main(String[] args) {
+        System.out.println(0101);
+        System.out.print(0101);
+    }
+}
